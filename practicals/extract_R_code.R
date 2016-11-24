cacd93bd9bb7a3bdc4713d4d09460560b91c4b4c
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2016-11-24 08:30:51 +0100

    added

---
+++ practicals/extract_R_code.R
@@ -0,0 +1,11 @@
+################################################################
+## Extract the code of this markdown file
+library(knitr)
+this.folder <- "~/stats_avec_RStudio_EBA/practicals/02_peak-calling/"
+setwd(this.folder)
+md.file <- "peak-calling_stats_practical.Rmd"
+purl(md.file)
+
+  
+  
+  
