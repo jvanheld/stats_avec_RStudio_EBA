87a5d87afcfec51d8f746b7554523ccaaccdaa36
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2017-01-08 18:01:21 +0100

    automatic extraction of R files from all the Rmd files

--- practicals/extract_R_code.R
+++ practicals/extract_R_code.R
@@ -1,10 +1,22 @@
 ################################################################
 ## Extract the code of this markdown file
 library(knitr)
-this.folder <- "~/stats_avec_RStudio_EBA/practicals/02_peak-calling/"
-setwd(this.folder)
-md.file <- "peak-calling_stats_practical.Rmd"
-purl(md.file)
+
+dir.course <- "~/stats_avec_RStudio_EBA"
+dir.practicals <- file.path(dir.course, "practicals")
+Rmd.files <- list.files(path=dir.practicals, pattern="*.Rmd", recursive = TRUE)
+setwd(dir.practicals)
+
+for (md.file in Rmd.files) {
+  R.file <- sub(pattern=".Rmd$", replacement = ".R", x = md.file)
+  purl(md.file, output=R.file, quiet=TRUE)
+  message("Extracted R code: ", R.file)
+}
+
+# this.folder <- "~/stats_avec_RStudio_EBA/practicals/02_peak-calling/"
+# setwd(this.folder)
+# md.file <- "peak-calling_stats_practical.Rmd"
+# purl(md.file)
 
   
   
